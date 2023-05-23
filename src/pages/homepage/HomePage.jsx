90afc735a8f570ab30d7f161d05cf6e2a054a33b
bruno <truongtritin.bee@gmail.com>
2023-05-24 00:51:23 +0700

    This is changing the homepage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -119,32 +119,43 @@ const HomePage = () => {
       <div className="w-full h-full text-white z-10 absolute flex items-center">
         <div className="m-3 sm:m-24">
           <h1 className="font-bold text-6xl py-3 text-left">Bruno Truong</h1>
-          <p className="text-xl rounded-xl py-1  text-left w-3/4 ">{`< Front-End-Developer />`}</p>
-          <div className="grid sm:flex gap-3 py-3">
+          <p className="text-xl rounded-xl py-1  text-left w-3/4 ">{`< Front-End Developer />`}</p>
+          <div className="grid sm:flex gap-3 py-3 sm:w-full">
             <div className="bg-white w-fit px-3 italic text-blue-900 font-medium py-1 rounded-3xl">
               <h1>+ Think organization</h1>
-            </div>{" "}
-            <div className="bg-white w-fit px-3 italic text-blue-900 font-medium py-1 rounded-3xl">
-              <h1>+ Code consistently</h1>
             </div>
             <div className="bg-white w-fit px-3 italic text-blue-900 font-medium py-1 rounded-3xl">
-              <h1>+ Positive attitude</h1>
+              <h1>+ Quick Learning</h1>
+            </div>
+            <div className="bg-white w-fit px-3 italic text-blue-900 font-medium py-1 rounded-3xl">
+              <h1>+ Open Minded</h1>
             </div>
           </div>
-          <div className=" flex items-center text-center w-3/4 ">
-            <button className="border hover:bg-white hover:text-blue-900 active:bg-persian-600 active:text-white border-blue-900 px-3 py-1 rounded-xl">
-              <a download href={JuniorReactJS}>
-                Resume
-              </a>
-            </button>
-            <div className="flex pl-3 items-center gap-3 justify-center">
-              {contactsIcon.map((icon) => (
-                <button className=" active:text-persian-600" key={icon.name}>
-                  <a rel="noreferrer" href={icon.link} target="_blank">
-                    {icon.icon}
-                  </a>
-                </button>
-              ))}
+          <div className=" flex  flex-col gap-3 items-center text-center xl:w-1/2 h-50  bg-blue-900 bg-opacity-10 rounded-3xl ">
+            <p className="w-3/4 pt-3">
+              Hi there, welcome to my personal website.
+              <br /> As a FrontEnd Developer, patience and passion are my
+              strength, creativity and quick learning are my hobbies. And with
+              those, everything is possible.
+            </p>
+            <div>
+              <button className="border hover:bg-white hover:text-blue-900 active:bg-persian-600 active:text-white border-blue-900 px-3 py-1 rounded-xl">
+                <a download href={JuniorReactJS}>
+                  Resume
+                </a>
+              </button>{" "}
+              <button className="border hover:bg-white hover:text-blue-900 active:bg-persian-600 active:text-white border-blue-900 px-3 py-1 rounded-xl">
+                Let's talk
+              </button>
+              <div className="flex pl-3 items-center pt-3 pb-3 gap-3 justify-center">
+                {contactsIcon.map((icon) => (
+                  <button className=" active:text-persian-600" key={icon.name}>
+                    <a rel="noreferrer" href={icon.link} target="_blank">
+                      {icon.icon}
+                    </a>
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
         </div>
